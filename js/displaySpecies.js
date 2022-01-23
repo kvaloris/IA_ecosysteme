24d7682fd077259fbc162ff3986c9240157823ac
Natacha <natachaliao32@gmail.com>
2022-01-23 13:27:07 +0100

    css + display correctly

--- js/displaySpecies.js
+++ js/displaySpecies.js
@@ -1,28 +1,172 @@
 import * as THREE from './../node_modules/three/build/three.module.js';
+import { loadObject, animate, resizeRendererToDisplaySize } from './../main.js';
+import { TrackballControls } from './../vendor_mods/three/examples/jsm/controls/TrackballControls.js';
 
-export function displaySpecies() {
-    console.log(FishShoal.getFishesBySpecies());
+let idAnim2;
 
-    const div = document.createElement('div');
-    div.id = 'species-display';
 
-    displayFishInSpeciesDisplay(FishShoal.fishesArray[0]);
+export function displaySpecies(idAnim) {
+    cancelAnimationFrame(idAnim);
 
-    function displayFishInSpeciesDisplay(fish) {
+    const display = document.querySelector('#display-2');
+    display.style.display = "inherit";
 
-        const canvas = document.createElement('canvas');
-        canvas.classList.add('fish-display');
-        div.appendChild(canvas);
+    createSpeciesTab();
 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 100 );
+    const firstSpecie = Object.keys(FishShoal.getFishesBySpecies())[0];
+    const fishesOfFirstSpecie = FishShoal.getFishesBySpecies()[firstSpecie];
 
-        const renderer = new THREE.WebGLRenderer({ canvas });
-        renderer.setSize( 200, 200 );
-        document.body.appendChild( renderer.domElement );
+    displayFishesAsItems(fishesOfFirstSpecie);
+}
 
-        displayFish(fish);
+function displayFishesAsItems(fishes) {
 
-        renderer.render( scene, camera );
+    let prevGridFish = document.querySelector('.grid-fishes');
+    if (prevGridFish) prevGridFish.remove();
+
+    const container = document.querySelector('#container-2');
+
+    const grid = document.createElement('div');
+    grid.classList.add('grid-fishes');
+    container.appendChild(grid);
+
+    const canvas = document.createElement('canvas');
+    canvas.id = 'canvas-2';
+    grid.appendChild(canvas);
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
+
+    const sceneElements = [];
+    function addScene(elem, fn) {
+        sceneElements.push({ elem, fn });
     }
+
+    function makeScene(elem) {
+        const scene = new THREE.Scene();
+
+        const fov = 45;
+        const aspect = 2;  // the canvas default
+        const near = 0.1;
+        const far = 5;
+        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        camera.position.set(0, 1, 2);
+        camera.lookAt(0, 0, 0);
+        scene.add(camera);
+
+        const controlsFish = new TrackballControls(camera, elem);
+        controlsFish.noZoom = true;
+        controlsFish.noPan = true;
+
+        {
+            const color = 0xFFFFFF;
+            const intensity = 1;
+            const light = new THREE.DirectionalLight(color, intensity);
+            light.position.set(-1, 2, 4);
+            camera.add(light);
+        }
+
+        return { scene, camera, controlsFish };
+    }
+
+    for (let i = 0; i < fishes.length; i++) {
+        let fish = fishes[i];
+
+        const elem = document.createElement('span');
+        elem.classList.add('diagram');
+        elem.id = 'list-fish-' + i;
+        grid.appendChild(elem);
+
+        const { scene, camera, controlsFish } = makeScene(elem);
+        displayFishAt(fish, scene, (fish.size - MINSIZE) / (MAXSIZE - MINSIZE) * (3 - 1) + 1, 0, 0, 0);
+
+        addScene(elem, (time, rect) => {
+            camera.aspect = rect.width / rect.height;
+            camera.updateProjectionMatrix();
+            controlsFish.handleResize();
+            controlsFish.update();
+            // mesh.rotation.y = time * .1;
+            renderer.render(scene, camera);
+        });
+    }
+
+    const clearColor = new THREE.Color('#000');
+    function render(time) {
+        time *= 0.001;
+
+        resizeRendererToDisplaySize(renderer);
+
+        renderer.setScissorTest(false);
+        renderer.setClearColor(clearColor, 0);
+        renderer.clear(true, true);
+        renderer.setScissorTest(true);
+
+        const transform = `translateY(${window.scrollY}px)`;
+        renderer.domElement.style.transform = transform;
+
+        for (const { elem, fn } of sceneElements) {
+            // get the viewport relative position of this element
+            const rect = elem.getBoundingClientRect();
+            const { left, right, top, bottom, width, height } = rect;
+
+            const isOffscreen =
+                bottom < 0 ||
+                top > renderer.domElement.clientHeight ||
+                right < 0 ||
+                left > renderer.domElement.clientWidth;
+
+            if (!isOffscreen) {
+                const positiveYUpBottom = renderer.domElement.clientHeight - bottom;
+                renderer.setScissor(left, positiveYUpBottom, width, height);
+                renderer.setViewport(left, positiveYUpBottom, width, height);
+
+                fn(time, rect);
+            }
+        }
+
+        idAnim2 = requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+}
+
+// Display the fish passed in parameter 
+function displayFishAt(fish, group, size, x, y, z) {
+    var fichierName;
+
+    switch (fish.color) {
+        case 0:
+            fichierName = "poisson2.glb"; // red
+            break;
+        case 1:
+            fichierName = "poisson.glb"; // blue
+            break;
+        default:
+            fichierName = "poisson3.glb"; // yellow
+            break;
+    }
+
+    loadObject(size, fichierName, x, y, z, group);
+}
+
+function createSpeciesTab() {
+    const div = document.querySelector('.species-tabs');
+    div.innerHTML = "";
+    const species = Object.keys(FishShoal.getFishesBySpecies());
+    species.forEach(specie => {
+        let span = document.createElement('span');
+        span.innerText = specie;
+        div.appendChild(span);
+        let fishes = FishShoal.getFishesBySpecies()[specie];
+        span.addEventListener('click', () => {
+            const tabs = div.childNodes;
+            tabs.forEach(tab => tab.classList.remove("selected"));
+            span.classList.add("selected");
+            displayFishesAsItems(fishes);
+        })
+    })
+}
+
+export function closeSpeciesDisplay() {
+    cancelAnimationFrame(idAnim2);
+    const display = document.querySelector('#display-2');
+    display.style.display = "none";
+    animate();
 }
---
+++ main.js
@@ -0,0 +1,174 @@
+import * as THREE from './node_modules/three/build/three.module.js';
+import { OrbitControls } from './vendor_mods/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './vendor_mods/three/examples/jsm/loaders/GLTFLoader.js';
+import { displaySpecies, closeSpeciesDisplay } from './js/displaySpecies.js';
+// import { GUI } from 'dat.gui';
+
+const scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+      /*fov*/ 75,
+      /*aspect ratio*/ window.innerWidth / window.innerHeight,
+      /*near*/ 0.1,
+      /*far*/ 1000
+);
+camera.position.x = 200;
+camera.position.y = 250;
+camera.position.z = 396;
+
+const canvas = document.querySelector('#canvas-1');
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+document.querySelector("#display-1").appendChild(renderer.domElement);
+
+const fishesGroup = new THREE.Group();
+
+const loader = new THREE.TextureLoader();
+scene.background = loader.load('./images/sea.jpg');
+
+// Load 3D object fish at the size asked and at the position asked
+export function loadObject(size, nomPoisson, x, y, z, group) {
+  var loader = new GLTFLoader();
+  loader.load('./3dobjects/' + nomPoisson, function (gltf) {
+    let poisson = gltf.scene;
+    poisson.scale.set(size, size, size);
+    poisson.position.set(x, y, z);
+
+    group.add(poisson);
+  }, undefined, function (error) {
+    console.error(error);
+  });
+}
+
+// Allow to empty a group of fishes
+function deleteGroup() {
+  for (var i = fishesGroup.children.length - 1; i >= 0; i--) {
+    fishesGroup.remove(fishesGroup.children[i]);
+  }
+}
+
+// Display the fish passed in parameter 
+function displayFish(fish, group) {
+  var fichierName;
+
+  switch (fish.color) {
+    case 0:
+      fichierName = "poisson2.glb"; // red
+      break;
+    case 1:
+      fichierName = "poisson.glb"; // blue
+      break;
+    default:
+      fichierName = "poisson3.glb"; // yellow
+      break;
+  }
+
+  loadObject(fish.size, fichierName, fish.x, fish.y, fish.z, group);
+}
+
+// Display a certain number of fishes
+function displayFishes(group) {
+  for (let i = 0; i < FishShoal.fishesArray.length; i++) {
+    displayFish(FishShoal.fishesArray[i], group);
+  }
+}
+
+scene.add(fishesGroup);
+
+// CONTROL
+
+var controls = new OrbitControls(camera, renderer.domElement);
+
+// LIGHT
+
+const pointLight = new THREE.PointLight(0x00ff00, 2, 50);
+const directionalLight = new THREE.DirectionalLight(0xffffff);
+const ambientLight = new THREE.AmbientLight(0x111111);
+scene.add(ambientLight);
+scene.add(pointLight);
+scene.add(directionalLight);
+
+
+// SLIDERS
+
+const slider_s = document.querySelector(".slider-separation");
+const slider_ag = document.querySelector(".slider-cohesion");
+const slider_al = document.querySelector(".slider-alignment");
+const slider_mutChance = document.querySelector(".slider-mutChance");
+
+slider_ag.addEventListener('change', () => {
+  c_ag = slider_ag.value;
+})
+slider_s.addEventListener('change', () => {
+  c_s = slider_s.value;
+})
+slider_al.addEventListener('change', () => {
+  c_al = slider_al.value;
+})
+
+slider_mutChance.addEventListener('change', () => {
+  FishShoal.setMutChance(slider_mutChance.value);
+  console.log("val slider" + slider_mutChance.value);
+})
+
+let c_ag = slider_ag.value; let c_s = slider_s.value; let c_al = slider_al.value;
+
+//BUTTONS
+var btnNextYear = document.querySelector(".button-next-year");
+//console.log(btnNextYear);
+if (btnNextYear != null) {
+  btnNextYear.addEventListener('click', () => {
+    FishShoal.nextYear();
+    deleteGroup();
+    displayFishes(fishesGroup);
+    document.querySelector('.nbPoisson').innerHTML = FishShoal.getNbFishToString();
+  });
+}
+
+
+// ANIMATION
+
+let idAnim;
+
+export function animate() {
+
+  resizeRendererToDisplaySize(renderer);
+
+  idAnim = requestAnimationFrame(animate);
+  FishShoal.updatePosition(c_ag, c_s, c_al, fishesGroup);
+
+  // Here, the code for animation
+
+  renderer.render(scene, camera);
+}
+
+FishShoal.init(100);
+displayFishes(fishesGroup);
+
+animate();
+document.addEventListener('keydown', () => {
+  FishShoal.nextYear();
+  deleteGroup();
+  displayFishes(fishesGroup);
+});
+
+FishShoal.setMutChance(slider_mutChance.value);
+//TEXT INFORMATION
+document.querySelector('.nbPoisson').innerHTML = FishShoal.getNbFishToString();
+
+
+export function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+      renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+const speciesButton = document.getElementById('display-species');
+speciesButton.addEventListener('click', () => displaySpecies(idAnim));
+
+document.querySelector('#close-display-2').addEventListener('click', closeSpeciesDisplay);
+
